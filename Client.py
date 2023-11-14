abc8c6f83e6f91ab041b93c9b652a9032f4f1e86
yoni kisho <yonatanreichert@gmail.com>
2023-11-14 19:48:44 +0200

    Fixed little things in both files, made better function dividing, assertion, documentation and more

--- Client.py
+++ Client.py
@@ -15,6 +15,8 @@ SERVER_ADDRESS = ('127.0.0.1', 1729)
 logging.basicConfig(filename='client.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('client')
 
+VALID_COMMANDS = ("time", "name", "rand", "exit")
+
 
 def get_message(my_socket):
     """
@@ -37,13 +39,33 @@ def validate_message(message):
     @:return: True if message is valid, False otherwise.
     """
     message = message.lower()  # for easier parsing
-    if len(message) > 4:
-        logger.error(f"Client's message too long ({len(message)})")
-        return False
-    if message in ("time", "name", "rand", "exit"):
+    if message in VALID_COMMANDS:
         return True
     # if message doesn't match any command, return un-valid
+    logger.info(f"User tried to enter un-valid command, ({message})")
     return False
+
+
+def send_messages_to_server(client_socket):
+    """
+    send a message to the server and print response
+    :param client_socket:
+    :return:
+    """
+    try:
+        while True:
+            message = input("Enter message:")
+            if message == 'exit':
+                return
+            # making sure the message is okay
+            if validate_message(message):
+                client_socket.send(message.encode())
+                response = get_message(client_socket)
+                print(response)
+            else:
+                print("You entered an un-valid message, try again or type 'exit' to exit")
+    except socket.error as err:
+        logger.error('Received socket error: %s', err)
 
 
 def main():
@@ -52,29 +74,21 @@ def main():
     @:return: None
     @:raises: socket.error on socket-related errors.
     """
-    assert(validate_message("time"))
-    assert(not validate_message("t1m#"))
-    assert(not validate_message("neme"))
-    assert(validate_message("exit"))
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        my_socket.connect(SERVER_ADDRESS)
-        logger.info(f"Manage to connect to the server at: {SERVER_ADDRESS}")
-        message = ""
-        while message != "exit":
-            message = input("Enter message: ")
-            if validate_message(message):
-                my_socket.send(message.encode())
-                response = get_message(my_socket)
-                print(response)
-            else:
-                print("You entered an un-valid message, try again or type 'exit' to exit")
+        client_socket.connect(SERVER_ADDRESS)
+        logger.info(f"Managed to connect to the server at: {SERVER_ADDRESS}")
+        send_messages_to_server(client_socket)
     except socket.error as err:
         logger.error('Received socket error: %s', err)
     finally:
-        my_socket.close()
+        client_socket.close()
         logger.info("Client socket closed.")
 
 
 if __name__ == "__main__":
+    assert(validate_message("time"))
+    assert(not validate_message("t1m#"))
+    assert(not validate_message("hello"))
+    assert(validate_message("exit"))
     main()
--- server.py
+++ server.py
@@ -20,17 +20,21 @@ logger = logging.getLogger('server')
 
 
 def handle_message(message):
-    if message == 'time':
-        response = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    elif message == 'name':
-        response = "My name is Inigo Montoya"
-    elif message == "rand":
-        response = str(random.randint(1, 10))
-    else:
-        logger.error("Client sent unknown word")
-        response = "You sent an unknown command, try again or type 'exit' to exit"
-    # server doesn't need to check to un-valid message since client is responsible for checking
-
+    """
+    convert client's command into server's response
+    :param message:
+    :return:
+    """
+    match message:
+        case 'time':
+            response = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        case 'name':
+            response = "My name is Inigo Montoya"
+        case 'rand':
+            response = str(random.randint(1, 10))
+        case _:
+            logger.error("Client sent unknown word")
+            response = "You sent an unknown command, try again or type 'exit' to exit"
     response = str(len(response)) + "!" + response
     return response
 
@@ -42,7 +46,6 @@ def handle_client(client_socket):
     @:return: None
     @:raises: socket error if there's an error in receiving data.
     """
-
     while True:
         client_input = client_socket.recv(4).decode()
         if client_input.lower() == 'exit':
@@ -53,38 +56,48 @@ def handle_client(client_socket):
         client_socket.send(response.encode())
 
 
+def accept_client(server_socket):
+    """
+    accept client and get his messages until disconnected
+    :param server_socket:
+    :return:
+    """
+    client_socket, client_address = server_socket.accept()
+    try:
+        logger.info(f"Accepted connection from {client_address[0]}: {client_address[1]}")
+        handle_client(client_socket)
+    except socket.error as err:
+        logger.error('Received error while handling client: %s', err)
+        client_socket.close()
+
+
 def main():
     """
     Initialize the server socket, accept incoming connections, and handle messages.
     @:return: None
     @:raises: socket.error on socket-related errors, KeyboardInterrupt when user interrupts the process.
     """
-    assert len(handle_message("time")) == 22
-    assert 0 < int(handle_message("rand")[2]) < 10
-    assert handle_message("name") == "24!My name is Inigo Montoya"
-    assert len(handle_message("invalid message")) == 64
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        my_socket.bind(SERVER_ADDRESS)
-        my_socket.listen(QUEUE_LEN)
+        # Starting server
+        server_socket.bind(SERVER_ADDRESS)
+        server_socket.listen(QUEUE_LEN)
         logger.info(f"Server is listening on {SERVER_ADDRESS[0]}: {SERVER_ADDRESS[1]}")
-        # Handle clients forever
+        # listen to clients forever
         while True:
-            client_socket, client_address = my_socket.accept()
-            try:
-                logger.info(f"Accepted connection from {client_address[0]}: {client_address[1]}")
-                handle_client(client_socket)
-            except socket.error as err:
-                logger.error('Received error while handling client: %s', err)
-                client_socket.close()
+            accept_client(server_socket)
     except socket.error as err:
         logger.error('Received socket error on server socket: %s', err)
     except KeyboardInterrupt:
         logger.info("Server was terminated by the user.")
     finally:
-        my_socket.close()
+        server_socket.close()
         logger.info("Server socket closed.")
 
 
 if __name__ == "__main__":
+    assert len(handle_message("time")) == 22
+    assert 0 < int(handle_message("rand")[2]) < 10
+    assert handle_message("name") == "24!My name is Inigo Montoya"
+    assert len(handle_message("invalid message")) == 64
     main()
