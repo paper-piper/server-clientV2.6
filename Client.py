c65d7cbdb8b7586f7c4ebf8ce4a1a1434686861e
yoni kisho <yonatanreichert@gmail.com>
2023-11-20 22:28:08 +0200

    First working version with some things left to do. 1. Add proper documentation for function 2. Fix some and track bugs (program stops when asked for some commands, need to check)

--- Client.py
+++ Client.py
@@ -13,7 +13,7 @@ import io
 MAX_PACKET = 1024
 SERVER_ADDRESS = ('127.0.0.1', 1729)
 
-# Set up logging
+# set up logging
 logging.basicConfig(filename='client.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('client')
 
